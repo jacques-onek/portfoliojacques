1909d571839dad1963a85e8595fc7751c9ffdd5b
jacques onek <onekongajaque@gmail.com>
2025-07-13 21:08:14 +0200

    je cree la section projects

---
+++ components/Projects.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import TitleItems from './TitleItems'
+import { ProjectsData } from '@/constant'
+import CardProject, { CardProjecProps } from './CarProject'
+
+const Projects = () => {
+  return (
+    <section className='section-wrapper flex flex-col' id='project'>
+        <div>
+            <TitleItems title='Projects' textSize='text-xl lg:text-6xl font-extrabold' className='w-[50%] lg:w-[75%] h-[2px]' />
+        </div>
+        <div className='w-full grid grid-cols-1 grid-rows-2 overflow-hidden lg:grid-cols-2 '>
+            { ProjectsData.map((project)=>(
+                <CardProject {...project} key={project.title} />
+            )) }
+        </div>
+    </section>
+  )
+}
+
+export default Projects
